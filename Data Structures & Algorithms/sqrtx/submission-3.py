7d542c214e2ebe4221e01208397de8feab6f371d
arten05 <143962953+arten05@users.noreply.github.com>
2026-07-13 18:14:19 +0300

    Add: sqrtx - submission-3

---
+++ Data Structures & Algorithms/sqrtx/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        left = 1
+        right = x
+        res = []
+        while right - left > 1:
+            mid = (left + right) // 2
+            if mid ** 2 <= x:
+                res.append(mid)
+                left = mid
+            elif mid ** 2 > x:
+                right = mid
+        return left
+
+        
